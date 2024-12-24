89568edb4526ee6f1068fc949217c7618a625439
takuya-sangil <jayloveswonyoung@gmail.com>
2024-12-25 02:46:42 +0800

    added validation test

--- api/validation.py
+++ api/validation.py
@@ -1,5 +1,11 @@
+from dotenv import load_dotenv
 import os
 
+# Load .env file
+dotenv_loaded = load_dotenv()
+print(f"Dotenv loaded: {dotenv_loaded}")
+
+# Print environment variables
 print(f"AWS_ACCESS_KEY: {os.getenv('AWS_ACCESS_KEY')}")
 print(f"AWS_SECRET_KEY: {os.getenv('AWS_SECRET_KEY')}")
 print(f"BUCKET_NAME: {os.getenv('BUCKET_NAME')}")
